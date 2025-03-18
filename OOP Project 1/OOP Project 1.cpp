933aa837f59b9c47b2c877550989a3e3345f4443
veqr <148455497+veqr@users.noreply.github.com>
2025-03-18 21:23:45 +0000

    Twelfth Commit
    Added the last few menus and functions to finish off the code before commenting it all

--- OOP Project 1/OOP Project 1.cpp
+++ OOP Project 1/OOP Project 1.cpp
@@ -222,6 +222,7 @@ int login() {
 }
 
 void showBorrowedBooks() {
+
     cout << "You are currently borrowing:\n";
     for (int i = 1; i < memberBooks->borrowedBooks.size(); i++) {
         string bookname = memberBooks->borrowedBooks[i].name;
@@ -231,6 +232,7 @@ void showBorrowedBooks() {
 }
 
 void showReservedBooks() {
+
     cout << "You are currently reserving:\n";
     for (int i = 1; i < memberBooks->reservedBooks.size(); i++) {
         string bookname = memberBooks->reservedBooks[i].name;
@@ -240,6 +242,7 @@ void showReservedBooks() {
 }
 
 void returnScreen() {
+
     int bookchoice;
     bool found = false;
     for (int i = 1; i < memberBooks->borrowedBooks.size(); i++) {
@@ -263,12 +266,46 @@ void returnScreen() {
     else {
         cout << "This book has not been found.\n";
     }
+}
 
-
+void searchBooks() {
     
+    string booksearch;
+    cout << "Please enter the name of a book you wish to search by name or the author you wish to search for, replace any spaces with an underscore.\n";
+    cin >> booksearch;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int booknum = 0; booknum < currentBooksList->books.size(); booknum++) {
+        if (booksearch == currentBooksList->books[booknum].name) {
+            string takentxt;
+            if (currentBooksList->books[booknum].taken == 1) {
+                takentxt = "Borrowed";
+            }
+            else {
+                takentxt = "Available";
+            }
+
+
+            cout << "Name: " << currentBooksList->books[booknum].name << ", Author: " << currentBooksList->books[booknum].author << ", Availability: " << takentxt << "\n";
+        }
+        if (booksearch == currentBooksList->books[booknum].author) {
+            string takentxt;
+            if (currentBooksList->books[booknum].taken == 1) {
+                takentxt = "Borrowed";
+            }
+            else {
+                takentxt = "Available";
+            }
+
+
+            cout << "Name: " << currentBooksList->books[booknum].name << ", Author: " << currentBooksList->books[booknum].author << ", Availability: " << takentxt << "\n";
+        }
+    }
 }
 
 void borrowScreen() {
+
     int bookchoice;
     cout << "Enter a number from the book list below to borrow a book.\n";
     bookList();
@@ -289,6 +326,7 @@ void borrowScreen() {
 }
 
 void reservedScreen() {
+
     int bookchoice;
     cout << "Enter a number from the book list below to reserve a book.\n";
     bookList();
@@ -305,6 +343,59 @@ void reservedScreen() {
     }
 }
 
+void addBook() {
+
+    string newname;
+    string newauthor;
+    cout << "Please enter a new book name\n";
+    cin >> newname;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Please enter a new book author\n";
+    cin >> newauthor;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    currentBooksList->books.emplace_back(newname, newauthor, false);
+
+    bookList();
+}
+
+void removeBook() {
+    int delbook;
+    cout << "Enter the number to delete a book.\n";
+    for (int i = 0; i < currentBooksList->books.size(); i++) {
+        cout << i << ", Name: " << currentBooksList->books[i].name << ", Author: " << currentBooksList->books[i].author;
+    }
+    cin >> delbook;
+    currentBooksList->books[delbook].name;
+    currentBooksList->books[delbook].author;
+    currentBooksList->books[delbook].taken;
+    cout << "This is the new list of books:\n\n";
+    bookList();
+}
+
+void updateBook() {
+    int updatebook;
+    cout << "Enter the number to update a book's status.\n";
+    for (int i = 0; i < currentBooksList->books.size(); i++) {
+        cout << i << ", Name: " << currentBooksList->books[i].name << ", Author: " << currentBooksList->books[i].author;
+    }
+    cin >> updatebook;
+    currentBooksList->books[updatebook].taken != currentBooksList->books[updatebook].taken;
+    cout << "Status ahs been updated.\n\n";
+    bookList();
+    
+}
+
+void borrowedList() {
+    for (int i = 0; i < currentBooksList->books.size(); i++) {
+        if (currentBooksList->books[i].taken == true) {
+            cout << i << ", Name: " << currentBooksList->books[i].name << ", Author: " << currentBooksList->books[i].author << ", Currently being borrowed.\n";
+        }
+        
+    }
+}
+
 void setBorrowLimit() {
     int templim;
     cout << "Please enter the borrowing limit\n";
@@ -315,23 +406,35 @@ void setBorrowLimit() {
 
 void setLatePenalty() {
     float temppen;
-    cout << "Please enter the late penalty\n";
+    cout << "Please enter the late penalty.\n";
     cin >> temppen;
     cout << "The late penalty has been set to " << temppen;
     latePenalty = temppen + 1;
 }
 
 void manageAccounts() {
-    cout << "Listed below are all the users in a numerical order\n";
+    int accountDel;
+    cout << "Listed below are all the users in a numerical order.\n";
     for (int i = 0; i < userPasswordVector.size(); i++) {
-        cout << i << " ," << userPasswordVector[i] << "\n";
+        cout << i << ", " << userPasswordVector[i] << "\n";
+    }
+    cout << "\nPlease enter the number of the user you would like to delete, enter -1 to return to the Admin Screen.\n";
+    cin >> accountDel;
+    switch (accountDel) {
+    case -1:
+        cout << "Cancelled\n";
+    default:
+        userEmailVector.erase(userEmailVector.begin() + accountDel);
+        userPasswordVector.erase(userPasswordVector.begin() + accountDel);
+        userTypeVector.erase(userTypeVector.begin() + accountDel);
+        cout << "Account Deleted\n";
     }
 }
 
 void adminScreen() {
     int choice;
     cout << "This is the Admin Screen.\n\n";
-    cout << "Please enter the number the select a choice\n\n1 to change the borrowing limit\n2 to change the late penalty\n3 to manage another account";
+    cout << "Please enter the number the select a choice.\n\n1 to change the borrowing limit.\n2 to change the late penalty.\n3 to manage another account.\n";
     cin >> choice;
     switch (choice) {
     case 1:
@@ -349,14 +452,33 @@ void adminScreen() {
 }
 
 void librarianScreen() {
-    cout << "Library";
+    int choice;
+    cout << "This is the Librarian Screen.\n\n";
+    cout << "Please enter the number to select a choice.\n\n1 to add a book.\n2 to remove a book.\n3 to update a book.\n4 to get a list of all borrowed books\n";
+    cin >> choice;
+    switch (choice) {
+    case 1:
+        addBook();
+        librarianScreen();
+    case 2:
+        removeBook();
+        librarianScreen();
+    case 3:
+        updateBook();
+        librarianScreen();
+    case 4:
+        borrowedList();
+        librarianScreen();
+    default:
+        cout << "Invalid choice";
+    }
 }
 
 void memberScreen() {
 
     int choice;
     cout << "This is the Member Screen.\n\n";
-    cout << "Please enter the number to select a choice\n\n1 to show the current list of available books.\n2 to borrow a book.\n3 to reserve a book.\n4 to return a book.\n5 to search for a book.\n6 to show what books you are currently borrowing.\n7 to show what books you are currently reserving.\n8 to go to the next day.\n9 to exit.\n";
+    cout << "Please enter the number to select a choice\n\n1 to show the current list of available books.\n2 to borrow a book.\n3 to reserve a book.\n4 to return a book.\n5 to search for a book.\n6 to show what books you are currently borrowing.\n7 to show what books you are currently reserving.\n8 to exit.\n";
     cin >> choice;
     switch (choice) {
     case 1:
@@ -375,6 +497,8 @@ void memberScreen() {
         returnScreen();
         memberScreen();
     case 5:
+        searchBooks();
+        memberScreen();
     case 6:
         showBorrowedBooks();
         memberScreen();
@@ -382,8 +506,6 @@ void memberScreen() {
         showReservedBooks();
         memberScreen();
     case 8:
-
-    case 9:
         exit(0);
     default:
         cout << "Invalid input, please try again.\n";
